1c557e1583dfef0caca9ffa98bdc1c960864c327
Harris Chuang <a0955826338@gmail.com>
2023-12-17 21:25:52 +0800

    getProducts 基礎實作(查詢列表條件,優化傳參數)

--- src/main/java/com/example/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/example/springbootmall/dao/impl/ProductDaoImpl.java
@@ -2,6 +2,7 @@ package com.example.springbootmall.dao.impl;
 
 import com.example.springbootmall.constant.ProductCategory;
 import com.example.springbootmall.dao.ProductDao;
+import com.example.springbootmall.dto.ProductQueryParams;
 import com.example.springbootmall.dto.ProductRequest;
 import com.example.springbootmall.model.Product;
 import com.example.springbootmall.rowmapper.ProductRowMapper;
@@ -24,21 +25,21 @@ public class ProductDaoImpl implements ProductDao {
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Override
-    public List<Product> getProducts(ProductCategory category, String search) {
+    public List<Product> getProducts(ProductQueryParams productQueryParams) {
         String sql = "select product_id, product_name, category, image_url" +
                 ", price, stock, description" +
                 ", created_date, last_modified_date from product WHERE 1=1";
 
         Map<String, Object> map = new HashMap<>();
 
-        if (category != null) {
+        if (productQueryParams.getCategory() != null) {
             sql += " AND category = :category";
-            map.put("category", category.name());
+            map.put("category", productQueryParams.getCategory().name());
         }
 
-        if (search != null) {
+        if (productQueryParams.getSearch() != null) {
             sql += " AND product_name LIKE :search ";
-            map.put("search", "%" + search + "%");
+            map.put("search", "%" + productQueryParams.getSearch() + "%");
         }
 
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
